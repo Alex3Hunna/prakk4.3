a3de59f61e5418d940b11c71de1ae224134b57c4
Admin <Admin@DESKTOP-7ADHF06>
2023-03-24 23:37:55 +0300

    Добавьте файлы проекта.

---
+++ prakk4.3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prakk4._3
+{
+    internal class Program
+    {
+        class Orcs
+        {
+            public int Gold { get; set; }
+            public int Quantity { get; set; }
+            public Orcs(int gold, int quantity)
+            {
+                Gold = gold;
+                Quantity = quantity;
+            }
+            public void inc()
+            {
+                Quantity++;
+                Gold += 2;
+                if (Quantity > 5)
+                {
+                    Gold += 2 * (Quantity - 5);
+                                               
+                }
+                Console.WriteLine("Получено " + Gold + " единиц золота");
+            }
+
+        }
+    
+    static void Main(string[] args)
+        {
+            Orcs army = new Orcs(0, 1);
+            for (int i = 1; i < 20; i++)
+            {
+                army.inc();
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
